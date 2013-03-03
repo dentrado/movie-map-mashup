131e28bb0a4c9aa67ca8f10b7070a3585545010e
Martin Forsgren <martin.forsgren@gmail.com>
2013-03-03 13:02:24 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,115 @@
+// == API URLS ==
+var tileUrl = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+//var mqlreadUrl = "https://www.googleapis.com/freebase/v1/mqlread?callback=?";
+var mqlreadUrl = "http://api.freebase.com/api/service/mqlread?callback=?";
+var imageUrl = "https://usercontent.googleapis.com/freebase/v1/image/";
+var wpUrl = "http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?"; 
+
+// == Config ==
+var imageSize = "?maxwidth=150&maxheight=150";
+var popupOptions = { maxHeight:400, minWidth:300 };
+
+// == mql keys ==
+var geolocation = "/location/location/geolocation";
+var title_id = "/imdb/topic/title_id";
+
+var query = {
+    query: [{
+        "type": "/film/film_location",
+        "name": null,
+        "id":null,
+        "key": {
+            "namespace": "/wikipedia/en_id",
+            "value":        null,
+            "limit":1
+        },
+        "featured_in_films": [{
+            "name": null,
+            "/imdb/topic/title_id" : []
+        }],
+        "/location/location/geolocation": {
+            "limit":1,
+            "longitude": null,
+            "latitude":  null
+        },
+        "limit":2000
+    }]
+}
+
+// == For debug ==
+var locations;
+var wikidata;
+
+var map;
+
+function toImdbLink(film) { 
+    return "<a href='http://www.imdb.com/title/" + film[title_id][0] + "'>"
+               + film.name +
+           "</a>";
+}
+
+function getSummary(wpId, callback) {
+    $.getJSON(wpUrl, {pageid:wpId, prop:"text", },
+              function(data) {
+                  var wpText = data.parse.text["*"];
+                  // use the first paragraph as summary:
+                  //callback( $(wpText).filter("p").first() );
+                  callback( wpText.match(/<p>.*<\/p>/)[0] );
+ });
+}
+
+function showPopup(marker, filmLocation) {
+    var wpId = filmLocation.key.value;
+    getSummary(wpId, function(summary) {
+        console.log("creating popup content");
+        var header = "<h2>" + filmLocation.name + "</h2>"; 
+        var image = "<img src='" + imageUrl + filmLocation.id + imageSize + "'></img>";
+        var filmList = 
+            "<h3>Movies filmed here:</h3>" +
+            "<ul><li>" + 
+            filmLocation.featured_in_films.map(toImdbLink).join("</li><li>");
+            + "</li></ul>";
+
+        marker.bindPopup("<div class='summary'>" 
+                         + header + image + summary + 
+                         "</div>"
+                         + filmList, 
+                         popupOptions)
+            .openPopup();
+    });
+}
+
+function addMarker(filmLocation) {
+    var coords = filmLocation[geolocation];
+    if(coords.latitude && coords.longitude) { 
+        L.circleMarker([coords.latitude, coords.longitude], {radius:5})
+            .addTo(map)
+            .on("click", function(e) {
+                // Don't create the popups until clicking to avoid
+                // unnecessary work.
+                showPopup(e.target, filmLocation);
+            });
+    } else {
+        console.log(filmLocation.name + " had no coordinates");
+    }
+}
+
+function init() {
+    // Initialize map:
+    map = L.map("map").setView([51.505, -0,09,], 3);    
+    L.tileLayer(tileUrl, {
+        attribution: 'Map data © OpenStreetMap contributors',
+        maxZoom: 18
+    }).addTo(map);
+
+    // Fetch filming location data and add markers:
+    $.getJSON(mqlreadUrl, 
+              { query: JSON.stringify(query)}, 
+              function(answer) {
+                  // todo: check status/code 
+                  locations = answer.result; // global to make debugging easier
+                  locations.forEach(addMarker);
+              });
+}
+
+window.addEventListener('load', init, false);
